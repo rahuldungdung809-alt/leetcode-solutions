000e665eb31adbda2299eb5b8c0c70722ecd3f60
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-07-24 22:49:38 +0530

    Time: 12 ms (42.72%) | Memory: 11.9 MB (54.50%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,7 +12,7 @@ public:
     bool hasCycle(ListNode *head){
         ListNode *slow=head;
         ListNode *fast=head;
-        while((fast!=NULL) && (fast->next !=NULL)){
+        while((fast!=nullptr) && (fast->next !=nullptr)){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
