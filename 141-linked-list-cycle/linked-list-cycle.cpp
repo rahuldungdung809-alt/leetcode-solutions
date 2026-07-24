eb7690cdae7a6f86c5e09624cdafd43fb14ee3b7
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-07-24 22:45:24 +0530

    Time: 12 ms (42.72%) | Memory: 11.9 MB (54.50%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    //OPTIMAL SOLUTION (FLOYD'S CYCLE DETECTION ALGORITHM)
+    bool hasCycle(ListNode *head){
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while((fast!=NULL) && (fast->next !=NULL)){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    // BRUTE FORCE SOLUTION 
+    /*bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*>st;
+        while(head!=NULL){
+            if(st.find(head)!=st.end()){
+                return true;
+            }
+            st.insert(head);
+            head=head->next;
+        }
+        return false;*/
+    }
+};
