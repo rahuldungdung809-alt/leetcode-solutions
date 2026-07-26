39543b545877e9cd78a6d1076212612fc47b79fd
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-07-27 00:26:16 +0530

    Time: 57 ms (12.86%) | Memory: 75.8 MB (8.14%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,10 +9,73 @@
  * };
  */
 class Solution {
+
 public:
+    ListNode* merge(ListNode* left,ListNode* right){
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        while(left!=NULL && right!=NULL){
+            if(left->val<=right->val){
+                temp->next=left;
+                left=left->next;
+            }
+            else{
+                temp->next=right;
+                right=right->next;
+            }
+            temp=temp->next;
+        }
+        if(left!=NULL){
+            temp->next=left;
+        }
+        if(right!=NULL){
+            temp->next=right;
+        }
+        return dummy->next;
+
+    }
     ListNode* sortList(ListNode* head) {
+        if (head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* right=slow->next;
+        slow->next=NULL;
+        ListNode* left=head;
+        left=sortList(left);
+        right=sortList(right);
+        return merge(left,right);
+
+        //BETTER SOLUTION(INSERTION SORT)
+        /*if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* sorted=NULL;
+        while(head!=nullptr){
+            ListNode* curr=head;
+            head=head->next;
+            if(sorted==NULL || curr->val<sorted->val){
+                curr->next=sorted;
+                sorted=curr;
+            }
+            else{
+                ListNode* temp=sorted;
+                while(temp->next!=nullptr && temp->next->val <curr->val){
+                    temp=temp->next;
+                }
+                curr->next=temp->next;
+                temp->next=curr;
+            }
+        }
+        return sorted;*/
+        //********************************************
         //BRUTE FORCE SOLUTION (STORE VALUE AND SORT)
-        vector<int>arr;
+        /*vector<int>arr;
         ListNode* temp=head;
         while(temp!=nullptr){
             arr.push_back(temp->val);
@@ -25,6 +88,6 @@ public:
             temp=temp->next;
 
         }
-        return head;
+        return head;*/
     }
 };
