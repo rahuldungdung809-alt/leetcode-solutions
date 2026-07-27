218d282e6cb36ce4a0db295358e884333ead366c
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-07-27 10:27:08 +0530

    Time: 69 ms (20.02%) | Memory: 28.8 MB (13.77%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,8 +9,23 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //BETTER SOLUTION (USING HASH SET)
+        unordered_set<ListNode*>st;
+        ListNode* temp=headA;
+        while(temp!=nullptr){
+            st.insert(temp);
+            temp=temp->next;
+        }
+        temp=headB;
+        while(temp!=nullptr){
+            if(st.find(temp)!=st.end()){
+                return temp;
+            }
+            temp=temp->next;
+        }
+        return NULL;
         //BRUTE FORCE APPROACH (COMPARE A WITH B)
-        ListNode* tempA=headA;
+        /*ListNode* tempA=headA;
         while(tempA!=nullptr){
             ListNode* tempB=headB;
             while(tempB!=nullptr){
@@ -21,7 +36,7 @@ public:
             }
             tempA=tempA->next;
         }
-        return NULL;
+        return NULL;*/
         
     }
 };
