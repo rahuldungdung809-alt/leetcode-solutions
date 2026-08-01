86d4235c8aeb40469801c219f2eb443b70792bd7
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-01 23:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int t[23][23];
+    int solve(vector<int>& nums,int i,int j){
+        if (i>j){
+            return 0;
+        }
+        if(i==j){
+            return nums[i];
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        int take_i=nums[i]-solve(nums,i+1,j);
+        int take_j=nums[j]-solve(nums,i,j-1);
+
+        return t[i][j]=max(take_i,take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        memset (t,-1,sizeof(t));
+        return solve(nums,0,n-1)>=0;
+        
+    }
+};
