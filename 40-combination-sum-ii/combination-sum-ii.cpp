39d0a75e1f1e598714fd1f6cf2466343d4e4c68c
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-03 19:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (19.89%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<int>path;
+    void dfs(vector<int>& candidates,int target,int start){
+        if(target==0){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=start;i<candidates.size();i++){
+            if(i>start && candidates[i]==candidates[i-1]){
+                continue;
+            }
+            if(candidates[i]>target){
+                break;
+            }
+            path.push_back(candidates[i]);
+            dfs(candidates,target-candidates[i],i+1);
+            path.pop_back();
+        }
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        dfs(candidates,target,0);
+        return ans;
+    }
+};
