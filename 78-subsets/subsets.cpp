b32be26e2384e77f4bf02415fad0ac0f512df09f
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-03 20:09:05 +0530

    Time: 1 ms (43.24%) | Memory: 10.1 MB (37.93%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        
+        for(int mask=0;mask<(1<<n);mask++){
+            vector<int>temp;
+            for(int i=0;i<n;i++){
+                if(mask & (1<<i)){
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+        
+    }
+};
