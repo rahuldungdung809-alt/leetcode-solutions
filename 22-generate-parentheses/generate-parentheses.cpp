b806475e52f1bd4d790b0280ab93b5abd06707e0
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-03 15:56:09 +0530

    Time: 4 ms (32.28%) | Memory: 13.2 MB (77.50%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    vector<string>result;
+    void solve(string& curr,int n,int open,int close){
+        if(curr.length()==2*n){
+            result.push_back(curr);
+        }
+        if(open<n){
+            curr.push_back('(');
+            solve(curr,n,open+1,close);
+            curr.pop_back();
+        }
+        if(close<open){
+            curr.push_back(')');
+            solve(curr,n,open,close+1);
+            curr.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n){
+        string curr="";
+        int open=0;
+        int close=0;
+        solve(curr,n,open,close);
+        return result;
+    }
+    /*
     vector<string>ans;
     string curr;
     bool isValid(){
@@ -36,5 +60,5 @@ public:
         generate(n);
         return ans;
         
-    }
+    }*/
 };
