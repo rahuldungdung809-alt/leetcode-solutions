2c00c542e0a518e35acd48e4517f0a02e2a642c2
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-09 00:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (53.59%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        // USING XOR OPERATOR
+        /*int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            s[left]=s[left]^s[right];
+            s[right]=s[left]^s[right];
+            s[left]=s[left]^s[right];
+            left++;
+            right--;
+        }*/
         // USING SWAP OPERATION
         int len=s.size();
         int left=0,right=len-1;
