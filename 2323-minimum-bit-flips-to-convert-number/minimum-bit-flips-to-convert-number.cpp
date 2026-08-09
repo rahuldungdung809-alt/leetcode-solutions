0f44bc8ba39df85dd55d6ac9f8ee611ba7def8cb
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-09 11:17:13 +0530

    Time: 2 ms (1.95%) | Memory: 8 MB (27.32%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int x=start^goal;
+        int count=0;
+        while(x){
+            x &=(x-1);
+            count++;
+        }
+        return count;
+        /*int ans=start^goal;
+        int count=0;
+        for(int i=0;i<31;i++){
+            if(ans & (1<<i)){
+                count++;
+            }
+        }
+        return count;*/
+        
+    }
+};
