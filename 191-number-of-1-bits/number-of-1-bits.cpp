839d8e1c46f53ee0f2dc7e0045cbef5950138de1
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-09 10:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.29%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n != 0){
+            count++;
+            n=n & (n-1);
+        }
+        return count;
+
+
+        /*int count=0;
+        for (int i=0;i<32;i++){
+            if((n>>i)&1){
+                count++;
+            }
+        }
+        return count;*/
+        
+    }
+};
