7ce92f770184d8cd77775001b1ddab2c857ca7aa
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-09 23:22:55 +0530

    Time: 3 ms (27.05%) | Memory: 10 MB (37.86%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> ans;
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
-        
         for(int mask=0;mask<(1<<n);mask++){
             vector<int>temp;
             for(int i=0;i<n;i++){
@@ -14,6 +13,38 @@ public:
             ans.push_back(temp);
         }
         return ans;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*int n=nums.size();
+        for(int mask=0;mask<(1<<n);mask++){
+            vector<int>temp;
+            for(int i=0;i<n;i++){
+                if(mask & (1<<i)){
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;*/
         
     }
 };
