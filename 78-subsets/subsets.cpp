575d62a85dd11100c3de499e6f6c5924b6f14ed8
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-09 23:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (82.96%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
+    vector<vector<int> > ans;
+    vector<int> result;
+    void solve(int index,  vector<int> &nums) {
+        if(index >= nums.size()) {
+            ans.push_back(result);
+            return;
+        }
+        result.push_back(nums[index]);
+        solve(index + 1, nums);
+        result.pop_back();
+        solve(index + 1, nums);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
+        
+        
+        solve(0, nums);
+        return ans;
+    }
+    /*
+    vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {*/
+
+
+
+        /*
         int n=nums.size();
         for(int mask=0;mask<(1<<n);mask++){
             vector<int>temp;
@@ -12,7 +35,7 @@ public:
             }
             ans.push_back(temp);
         }
-        return ans;
+        return ans;*/
         
 
 
@@ -46,5 +69,5 @@ public:
         }
         return ans;*/
         
-    }
+    
 };
