89dd240a94c9f53e0340ce04626362f689c2bbf6
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-10 19:12:25 +0530

    Time: 2 ms (14.92%) | Memory: 9.2 MB (13.82%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        unordered_map<char,char>mp={{')','('},{']','['},{'}','{'}};
+        for(char c:s){
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+            else{
+                if(st.empty() || st.top()!=mp[c]){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+        
+    }
+};
