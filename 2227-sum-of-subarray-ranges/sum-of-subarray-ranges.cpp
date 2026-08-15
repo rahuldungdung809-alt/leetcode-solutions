c51d6ec5651b391833a550d0a50a4ea77869e431
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-15 16:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (91.10%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,11 +1,41 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {                                                                                    
+    long long subArrayRanges(vector<int>& nums) {         
+        int n=nums.size();
+        long long ans=0;
+        vector<int>st;
+
+        //Maximum contribution
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || nums[st.back()]<nums[i])){
+                int j=st.back();
+                st.pop_back();
+                int l=st.empty() ? j+1 : j-st.back();
+                int r=i-j;
+                ans+=1LL *nums[j]*l*r;
+            }
+            if(i<n)
+            st.push_back(i);
+        }
+        st.clear();
+        //Minimum contribution
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || nums[st.back()]>nums[i])){
+                int j=st.back();
+                st.pop_back();
+                int l=st.empty() ? j+1 : j-st.back();
+                int r=i-j;
+                ans-=1LL *nums[j]*l*r;
+            }
+            if(i<n)
+            st.push_back(i);
+        }    
+        return ans;                                                                       
                                                                                                                                                                                                                                                    
 
 
        // BRUTE FORCE TC:O(N^2) SC:O(1)
-        long long sum=0;
+        /*long long sum=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
             long long largest=nums[i],smallest=nums[i];
@@ -15,7 +45,7 @@ public:
                 sum+=(largest-smallest);
             }
         }
-        return sum;
+        return sum;*/
         
     }
 };
