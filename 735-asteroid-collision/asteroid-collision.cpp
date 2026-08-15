e1a4929abd21f7d5fade9b02e763ede49f1fba6d
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-15 11:16:18 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (89.96%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int>st;
+        vector<int>st;
+        for(int a:asteroids){
+            while(!st.empty() && a<0 && st.back()>0){
+                int sum=a+st.back();
+                if(sum<0){
+                    st.pop_back();
+                }
+                else if(sum>0){
+                    a=0;
+                }
+                else{
+                    st.pop_back();
+                    a=0;
+                }
+            }
+            if(a!=0){
+                st.push_back(a);
+            }
+        }
+        return st;
+        /*stack<int>st;
         for(int a:asteroids){
             while(!st.empty() && a<0 && st.top()>0){
                 int sum=a+st.top();
@@ -28,7 +48,7 @@ public:
             st.pop();
             i--;
         }
-        return res;
+        return res;*/
 
         
     }
