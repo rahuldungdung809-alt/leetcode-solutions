eb0613d74db98f0ef55bc6ce5e66ff05b99314d6
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-18 23:27:44 +0530

    Time: 4 ms (83.41%) | Memory: 71.5 MB (70.48%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,8 +1,27 @@
 class Solution {
 public:
+    int helper(vector<int>& nums,int k){
+        if(k<0)return 0;
+        int count=0;
+        int left=0;
+        
+        for(int right=0;right<nums.size();right++){
+            if(nums[right] %2==1){k--;}
+            while(k<0){
+                if(nums[left]%2==1){
+                    k++;
+                }
+                left++;
+            }
+            count+=right-left+1;
+        }
+        return count;
+    }
     int numberOfSubarrays(vector<int>& nums, int k) {
+        //OPTIMAL SOLUTION TC:O(N)   SC:O(1)
+        return helper(nums,k)-helper(nums,k-1);
         // BETTER SOLUTION TC:O(N)   SC:O(N)
-        unordered_map<int,int>freq;
+        /*unordered_map<int,int>freq;
         int oddCount=0;
         int res=0;
         freq[0]=1;
@@ -15,7 +34,7 @@ public:
             }
             freq[oddCount]++;
         }
-        return res;
+        return res;*/
 
         //BRUTE FORCE SOLUTION TC:O(N^2)   SC:O(1)
         /*int n=nums.size();
