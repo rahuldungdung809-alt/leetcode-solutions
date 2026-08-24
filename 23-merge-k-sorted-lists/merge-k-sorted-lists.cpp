cea4a34999277add623ab0b6e78b701bef5fed53
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-24 21:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (66.17%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,8 +10,43 @@
  */
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)
+            return list2;
+
+        if (list2 == nullptr)
+            return list1;
+
+        if (list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } 
+        else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+        return NULL;
+    }
+    ListNode* PartionandMerge(int start,int end,vector<ListNode*>& lists){
+        if(start>end){
+            return NULL;
+        }
+        if(start==end){
+            return lists[start];
+        }
+        int mid=start+(end-start)/2;
+        ListNode* L1=PartionandMerge(start,mid,lists);
+        ListNode* L2=PartionandMerge(mid+1,end,lists);
+        return mergeTwoLists(L1,L2);
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int>arr;
+        int k=lists.size();
+        if(k==0){
+            return NULL;
+        }
+        return PartionandMerge(0,k-1,lists);
+        //BRUTE FORCE SOLUTION TC:O(N LOG N)   SC:O(N)
+        /*vector<int>arr;
         for(ListNode* head:lists){
             while(head !=NULL){
                 arr.push_back(head->val);
@@ -25,7 +60,7 @@ public:
             temp->next=new ListNode(x);
             temp=temp->next;
         }
-        return dummy->next;
+        return dummy->next;*/
         
     }
 };
