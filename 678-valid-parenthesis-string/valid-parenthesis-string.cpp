25e63a04c37ada8c360129e5da55b63ac2986b73
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-27 22:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.73%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -26,6 +26,32 @@ public:
 
         // String is valid if all opens are closed
         return minOpen == 0;
+        /*
+        int minOpen = 0, maxOpen = 0;
+
+        // Traverse each character in the string
+        for (char c : s) {
+            if (c == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (c == ')') {
+                minOpen--;
+                maxOpen--;
+            } else {
+                // Treat '*' as '(', ')' or ''
+                minOpen--;
+                maxOpen++;
+            }
+
+            // If maxOpen goes negative, too many closing brackets
+            if (maxOpen < 0) return false;
+
+            // minOpen can't be negative
+            minOpen = max(minOpen, 0);
+        }
+
+        // String is valid if all opens are closed
+        return minOpen == 0;*/
         
     }
 };
