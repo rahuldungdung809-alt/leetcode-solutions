e4804ee89223e41e1fde9efbdddd5120f25d4292
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-08-30 21:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (65.19%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // GREEDY ALGORITH TC:O(N)  SC:O(1)
         int profit=0;
         for(int i=1;i<prices.size();i++){
             if(prices[i]>prices[i-1]){
