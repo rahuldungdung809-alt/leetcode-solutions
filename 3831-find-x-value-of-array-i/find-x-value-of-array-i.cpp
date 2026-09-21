bb9e8f7a8e69420f68960deb2701433b62b653e4
RAHUL DUNG DUNG <rahuldungdung809@gmail.com>
2026-09-21 23:43:45 +0530

    Time: 159 ms (61.61%) | Memory: 151.2 MB (55.36%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k, 0);
+        vector<long long> dp(k, 0);
+
+        for (int x : nums) {
+            vector<long long> newdp(k, 0);
+
+            // Start a new subarray [x]
+            newdp[x % k]++;
+
+            // Extend all previous subarrays
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    int newRemainder = (r * (x % k)) % k;
+                    newdp[newRemainder] += dp[r];
+                }
+            }
+
+            // Every subarray ending here contributes to answer
+            for (int r = 0; r < k; r++) {
+                ans[r] += newdp[r];
+            }
+
+            dp = newdp;
+        }
+
+        return ans;
+    }
+};
